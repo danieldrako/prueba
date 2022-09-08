df0d6c804e76d81f571c003c7c42fbb59d585263
danieldrako <ls.ortizdaniel@gmail.com>
2022-09-08 09:12:02 -0500

    cuarto

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,25 @@ function App() {
     <div className="App">
      
       <h1>
-      
+      <TypeWriterEffect
+        textStyle={{
+          fontFamily: 'Red Hat Display',
+          color: '#3F3D56',
+          fontWeight: 500,
+          fontSize: '1.5em',
+        }}
+        startDelay={2000}
+        cursorColor="#3F3D56"
+        multiText={[
+          'Hey there, This is a type writer animation package',
+          'it consist of two types...',
+          'Single text display and multi text display',
+          'Fonts can be customized.',
+          'The type speed can be customized as well',
+        ]}
+        multiTextDelay={1000}
+        typeSpeed={30}
+      > </TypeWriterEffect>
         Vite + React
         </h1>
        
