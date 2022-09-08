e522b0cca8630db2a3c29c6c5566839039d0e5d8
danieldrako <ls.ortizdaniel@gmail.com>
2022-09-08 09:03:56 -0500

    segundo

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,36 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
-
+import TypeWriterEffect from 'react-typewriter-effect';
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+     
+      <h1>
+      <TypeWriterEffect
+        textStyle={{
+          fontFamily: 'Red Hat Display',
+          color: '#3F3D56',
+          fontWeight: 500,
+          fontSize: '1.5em',
+        }}
+        startDelay={2000}
+        cursorColor="#3F3D56"
+        multiText={[
+          'Hey there, This is a type writer animation package',
+          'it consist of two types...',
+          'Single text display and multi text display',
+          'Fonts can be customized.',
+          'The type speed can be customized as well',
+        ]}
+        multiTextDelay={1000}
+        typeSpeed={30}
+      />
+        Vite + React
+        </h1>
+       
     </div>
   )
 }
